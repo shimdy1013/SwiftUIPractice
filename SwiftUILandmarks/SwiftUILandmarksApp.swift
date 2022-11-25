5ecc5f158dc2cd9fd4d894e19802a48e2b936de7
shimdy1013 <shimdy1013@naver.com>
2022-11-25 19:05:00 +0900

    Initial Commit

---
+++ SwiftUILandmarks/SwiftUILandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILandmarksApp.swift
+//  SwiftUILandmarks
+//
+//  Created by 심두용 on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
