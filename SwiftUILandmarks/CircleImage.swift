035b20c7aba273c8c8dc289c4acead3e46542484
shimdy1013 <shimdy1013@naver.com>
2022-11-27 04:04:07 +0900

    SwiftUIPractice

---
+++ SwiftUILandmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  SwiftUILandmarks
+//
+//  Created by 심두용 on 2022/11/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ SwiftUILandmarks/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  SwiftUILandmarks
+//
+//  Created by 심두용 on 2022/11/27.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
